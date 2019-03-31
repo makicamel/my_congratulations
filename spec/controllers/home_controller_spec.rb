d088856ca7598c48a81fd0463cc7dd250caadb8c
makicamel <unright@gmail.com>
2019-03-31 14:52:52 +0900

    Add HomeController#index spec.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status '200'
+    end
+  end
 end
