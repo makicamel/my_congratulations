a9a15b6c9ffe84fb99f783c7e56f5711e1450bb4
makicamel <unright@gmail.com>
2019-04-03 23:45:10 +0900

    Add home#index.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
 
   def index
+    from, to = home_params[:from], home_params[:to]
+    url = ''
+    agent = Mechanize.new
+    page = agent.get("#{url}?eki1=#{from}&eki2=#{to}&S=検索")
+    @elements = page.search('.bk_result')
+  end
+
+  private
+  def home_params
+    params.permit(:from, :to)
   end
 end
