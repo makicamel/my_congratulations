f30113b0989cf4f957041c47015716c1074acd71
makicamel <unright@gmail.com>
2019-04-06 23:56:04 +0900

    Show departure_time and arrival_time.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 
   def index
+    # TODO 複数候補の対応
     engine = Rails.application.credentials.search_engine
     from, transfer, to = home_params[:from], home_params[:transfer], home_params[:to]
     agent = Mechanize.new
     page = agent.get("#{engine[:url]}?#{engine[:from]}=#{from}&#{engine[:transfer]}=#{transfer}&#{engine[:to]}=#{to}&#{engine[:search]}=検索")
-    @elements = page.search('.bk_result')
+    @elements = {}
+    @elements[:departure_time] = page.search('.data_tm b:nth-child(2)')
+    @elements[:arrival_time] = page.search('.data_tm b:nth-child(5)')
   end
 
   private
