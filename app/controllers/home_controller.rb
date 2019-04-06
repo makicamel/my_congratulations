ebfd71621970e287ae7583791b5b530097b2ee4c
makicamel <unright@gmail.com>
2019-04-06 23:31:37 +0900

    Add transfer form.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,14 @@ class HomeController < ApplicationController
 
   def index
     engine = Rails.application.credentials.search_engine
-    from, to = home_params[:from], home_params[:to]
+    from, transfer, to = home_params[:from], home_params[:transfer], home_params[:to]
     agent = Mechanize.new
-    page = agent.get("#{engine[:url]}?#{engine[:from]}=#{from}&#{engine[:to]}=#{to}&#{engine[:search]}=検索")
+    page = agent.get("#{engine[:url]}?#{engine[:from]}=#{from}&#{engine[:transfer]}=#{transfer}&#{engine[:to]}=#{to}&#{engine[:search]}=検索")
     @elements = page.search('.bk_result')
   end
 
   private
   def home_params
-    params.permit(:from, :to)
+    params.permit(:from, :transfer, :to)
   end
 end
