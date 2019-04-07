f04ca270be925a49cbf4f937242de6c2b4402a93
makicamel <unright@gmail.com>
2019-04-07 19:39:38 +0900

    Display multiple blocks of routes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,18 @@ class HomeController < ApplicationController
     from, transfer, to = home_params[:from], home_params[:transfer], home_params[:to]
     agent = Mechanize.new
     page = agent.get("#{engine[:url]}?#{engine[:from]}=#{from}&#{engine[:transfer]}=#{transfer}&#{engine[:to]}=#{to}&#{engine[:search]}=検索")
-    @elements = {}
-    @elements[:departure_time] = page.search('.data_tm b:nth-child(2)')
-    @elements[:arrival_time] = page.search('.data_tm b:nth-child(5)')
+    @blocks = {}
+    ids = page.search('#results').css('.bk_result')
+    @blocks = ids.map do |id|
+      {
+        time: id.search('.data_tm').inner_text,
+        departure_station: id.search('.eki_s .nm').inner_text,
+        transfer_station: id.search('.eki .nm')[1].inner_text,
+        arrive_station: id.search('.eki_e .nm').inner_text,
+        first_route: id.search('.rosen .tm')[0].inner_text,
+        second_route: id.search('.rosen .tm')[1]&.inner_text,
+      }
+    end
   end
 
   private
