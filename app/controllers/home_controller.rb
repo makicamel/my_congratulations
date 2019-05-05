fc367277a2fb6dbe054713c240dbd66a389c355c
makicamel <unright@gmail.com>
2019-05-05 23:52:59 +0900

    Change how to get information from mechanize to yasuri.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,24 +14,6 @@ class HomeController < ApplicationController
     page = agent.get(url)
     p "#{engine[:url]}?#{engine[:from]}=#{@query.from}&#{engine[:transfer]}=#{@query.transfer}&#{engine[:to]}=#{@query.to}&" +
     "Dym=#{time_value[:ym]}&Ddd=#{time_value[:dd]}&Dhh=#{time_value[:hh]}&Dmn1=#{time_value[:mn1]}&Dmn2=#{time_value[:mn2]}&#{engine[:search]}=検索"
-    @blocks = {}
-    ids = page.search('#results').css('.bk_result')
-    @blocks = ids.map do |id|
-      {
-        time: id.search('.data_tm').inner_text,
-        departure_station: id.search('.eki_s .nm').inner_text,
-        transfer_station: id.search('.eki .nm')[1].inner_text,
-        arrive_station: id.search('.eki_e .nm').inner_text,
-        first_route: id.search('.rosen .tm')[0].inner_text,
-        second_route: id.search('.rosen .tm')[1]&.inner_text,
-        first_line: id.search('.rosen .rn')[0].inner_text,
-        second_line: id.search('.rosen .rn')[1]&.inner_text,
-        first_home_begin: id.search('.eki_s .ph').inner_text,
-        first_home_end: id.search('.eki .ph div')[1].inner_text,
-        second_home_begin: id.search('.eki .ph div')[2]&.inner_text,
-        second_home_end: id.search('.eki_e .ph').inner_text,
-      }
-    end
 # TODO: 経由駅が複数になった時の対応、経由が徒歩だった時の対応
 # TODO: 経由駅が複数候補ある時の対応
     src = ''
@@ -39,7 +21,7 @@ class HomeController < ApplicationController
     root = Yasuri.yaml2tree(src)
     agent = Mechanize.new
     root_page = agent.get(url)
-    result = root.inject(agent, root_page)
+    @blocks = root.inject(agent, root_page).map{|hash| hash.each{|k,v| v.symbolize_keys! if v.is_a?(Hash)}.symbolize_keys}
   end
 
   private
